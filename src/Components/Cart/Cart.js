f29b63f98457176613abfe4025342a14084a4250
Marina Batina <mbatina.oat@gmail.com>
2023-06-28 19:46:02 +0300

    Add CartItem component

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = (props)=>{
     const cartCTX = useContext(CartContext);
@@ -9,8 +10,17 @@ const Cart = (props)=>{
     const totalAmount = `$${cartCTX.totalAmount.toFixed(2)}`;
     const hasItems = cartCTX.items.length > 0;
 
+    const cartItemRemoveHandler =(id) =>{};
+    const cartItemAddHandler = (item) =>{};
+
     const cartItems = <ul className={classes['cart-items']}>
-        {cartCTX.items.map(el =><li>{el.name}</li>)}</ul>
+        {cartCTX.items.map(el =><CartItem key = {el.id} 
+         name = {el.name} 
+        amount = {el.amount} 
+        price = {el.price}
+        onRemove = {cartItemRemoveHandler.bind(null, el.id)}
+        onAdd = {cartItemAddHandler.bind (null, el)}
+        />)}</ul>
 
     return(
         <Modal>
