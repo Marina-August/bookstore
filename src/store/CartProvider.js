5052e011348667722600da4253b3d125a4a8eef0
Marina Batina <mbatina.oat@gmail.com>
2023-06-28 22:05:28 +0300

    Fix item duplicate problem in the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,8 +9,26 @@ const defaultCartState = {
 
 const cartReducer = (state, action)=>{
     if (action.type === 'ADD'){
-        const updatedItems = state.items.concat(action.item);
+        
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const exsistingCartItemIndex = state.items.findIndex((item)=>item.id ===action.item.id);
+        const exsistingCartItem = state.items[exsistingCartItemIndex];
+
+        let updatedItems;
+
+        if (exsistingCartItem){
+            const updatedItem = {
+                ...exsistingCartItem,
+                amount: exsistingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[exsistingCartItemIndex] = updatedItem;
+        } else{
+            updatedItems = state.items.concat(action.item);
+        }
+
+
         return{
             items: updatedItems,
             totalAmount: updatedTotalAmount
