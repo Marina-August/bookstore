b03ee4da7db2d3c0b2ea766e89bb9dd124aa7f24
Marina Batina <mbatina.oat@gmail.com>
2023-07-11 14:20:12 +0300

    Add backend functionality

--- src/Components/Books/AvailableBooks.js
+++ src/Components/Books/AvailableBooks.js
@@ -1,45 +1,73 @@
+import { useState,useEffect,useCallback } from 'react';
 import classes from './AvailableBooks.module.css'
 import Card from '../UI/Card';
 import BookItem from './BookItem';
 
-const Books = [
-    {
-      id: 'b1',
-      name: 'Book1',
-      description: 'Very interesting and shoking story! ',
-      price: 25,
-    },
-    {
-      id: 'b2',
-      name: 'Book2',
-      description: 'A real story of life of the Queen!',
-      price: 30,
-    },
-    {
-      id: 'b3',
-      name: 'Book3',
-      description: 'Unforgettable adventures of three friends and the dog!',
-      price: 32,
-    },
-    {
-      id: 'b4',
-      name: 'Book4',
-      description: 'If you want to learn how to cook, you definately should buy it!',
-      price: 28,
-    },
-  ];
 
 const AvailableBooks = ()=>{
-    return(
-        <section className={classes.books}>
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchBooksHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://movies-database-server.onrender.com/books');
+      console.log(response);
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      const transformedBooks = data.map((bookData) => {
+        return {
+          id: bookData._id,
+          name: bookData.name,
+          description: bookData.description,
+          price: bookData.price
+        };
+      });
+      console.log(transformedBooks);
+      setBooks(transformedBooks);
+      // setBooks(data);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchBooksHandler();
+  }, [fetchBooksHandler]);
+
+  let content = <p>Found no books.</p>;
+
+  if (books.length > 0) {
+    content = 
+    <section className={classes.books}>
             <Card>
                 <ul>
-                    {Books.map( book => <BookItem key ={book.id} name = {book.name} 
+                    {books.map( book => <BookItem key ={book.id} name = {book.name} 
                     description = {book.description} price = {book.price} id = {book.id}/>)}
                 </ul>
             </Card>
-        
-        </section>
+     </section>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+
+    return(
+      <section className={classes.books}>{content}</section>
     )
 
 }
