91b98bbdd7de941255b9b2db92a51e0ba89015c5
Marina Batina <mbatina.oat@gmail.com>
2023-07-12 09:05:56 +0300

    Add a form for orders. Customize a scrollbar

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props)=>{
+    const [isCheckout, setIsCheckout] = useState(false);
+
     const cartCtx = useContext(CartContext);
 
     const totalAmount = cartCtx.totalAmount;
@@ -26,19 +29,30 @@ const Cart = (props)=>{
         onAdd = {cartItemAddHandler.bind (null, el)}
         />)}</ul>
 
+    const orderHandler = ()=>{
+        setIsCheckout((prevIsCheckout) => !prevIsCheckout);
+    }    
+
     return(
+       
         <Modal>
+            <div className={classes.main}>   
             {cartItems}
             <div className={classes.total}>
-                <span>Total Amount</span>
+                <span>Total Amount $</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
+           { !isCheckout && <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-                {hasItems && <button className={classes.button} onClick={props.onHideCart}>Order</button>}
-
+                {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
             </div>
+            }
+           { isCheckout && <Checkout onCancel ={orderHandler}/>}
+           </div>
         </Modal>
+        
+        
+
     )
 
 }
